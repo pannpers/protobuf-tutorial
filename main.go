bdb61c40170a6ffb8c599651794a16a66c2dfd7a
pannpers <pepperoni9@gmail.com>
2019-02-14 09:16:11 +0900

    feat: print Protocol Buffers content

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	pb "github.com/pannpers/protobuf-tutorial"
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+	pb "github.com/pannpers/protobuf-tutorial/protofiles"
 )
 
-
-
 func main() {
-	p := &pb.
+	p := &pb.Person{
+		Id:    1234,
+		Name:  "Jonh Doe",
+		Email: "jonh.doe@example.com",
+		Phones: []*pb.Person_PhoneNumber{
+			{Number: "0120-0000", Type: pb.Person_MOBILE},
+		},
+	}
+
+	p1 := &pb.Person{}
+	body, _ := proto.Marshal(p)
+	_ = proto.Unmarshal(body, p1)
+
+	fmt.Printf("Original struct loaded from proto file: %v\n", p)
+	fmt.Printf("Marshaled proto data: %v\n", body)
+	fmt.Printf("Unmarshaled struct: %v\n", p1)
 }
